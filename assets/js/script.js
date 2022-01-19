33dcf43b756db80c1c04171e7663823d0ec2e9be
LizMackle <liz.mackle@outlook.com>
2022-01-19 12:57:03 +0800

    local storage code updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,64 @@
 // load local storage values
-// $(document).ready(function() {})
+$(document).ready(function () {
 
 
+    // displays current date & time
+    var timeElement = $("#current-day");
+    timeElement.text(moment().format("dddd, Do MMM | h:mm:ss"))
 
+    setInterval(function () {
+        timeElement.text(moment().format("dddd, Do MMM | h:mm:ss"));
+    }, 1000);
 
-// displays current date & time
-var timeElement = $("#current-day");
-timeElement.text(moment().format("dddd, Do MMM | h:mm:ss"))
+    // hourly time blocks 8am - 5pm
+    // each time block is colour coded to indicate current or past
+    // .past - grey 
+    // .present - light pink
+    // .future - light blue
+    var hourTimer = moment().hour();
+    var hourNow = document.querySelectorAll("#currentHour");
+    var hourBgColor = document.querySelectorAll("textarea");
+    var times = [];
 
-setInterval(function () {
-    timeElement.text(moment().format("dddd, Do MMM | h:mm:ss"));
-}, 1000);
-
-// hourly time blocks 8am - 5pm
-// each time block is colour coded to indicate current or past
-// .past - grey 
-// .present - light pink
-// .future - light blue
-var hourTimer = moment().hour();
-var hourNow = document.querySelectorAll("#currentHour");
-var hourBgColor = document.querySelectorAll("textarea");
-var times = [];
-
-for (var i = 0; i < hourNow.length; i++) {
-    times.push(hourNow[i].dataset.hour);
-
-}
-
-function bgColor() {
     for (var i = 0; i < hourNow.length; i++) {
-        if (Number(hourTimer) > Number(times[i])) {
-            hourBgColor[i].setAttribute("style", "background-color: #c7c5c5");
-        }
+        times.push(hourNow[i].dataset.hour);
 
-        if (Number(hourTimer) === Number(times[i])) {
-            hourBgColor[i].setAttribute("style", "background-color: #fabdc6");
-        }
-        else { 
+    }
 
+    function bgColor() {
+        for (var i = 0; i < hourNow.length; i++) {
+            if (Number(hourTimer) > Number(times[i])) {
+                hourBgColor[i].setAttribute("style", "background-color: #c7c5c5");
+            }
+
+            if (Number(hourTimer) === Number(times[i])) {
+                hourBgColor[i].setAttribute("style", "background-color: #fabdc6");
+            }
+            else {
+
+            }
         }
     }
-}
-        
-bgColor();
 
-// event can be saved into local storage by clicking save button 
-// - grab text content 
-// - use hour as local storage key 
-$(document).on("click", ".save-button", function (event) {
-    console.log("this button works");
+    bgColor();
+
+    // event can be saved into local storage by clicking save button 
+    // - grab text content 
+    // - use hour as local storage key 
+    $(document).on("click", ".save-button", function (event) {
+        var taskText = $(this).parent().siblings(".col-8").children("textarea").val();
+        var taskTime = $(this).parent().siblings(".col-2").data("hour");
+        localStorage.setItem(taskText, taskTime);
+        // console.log("this button works");
+    });
+
+    // when page is refreshed, events persist
+    $(".row").each(function () {
+        var timeBlock = $(this).data("hour");
+        const existingEvent = localStorage.getItem(timeBlock);
+        if (existingEvent) {
+            $(this).find("textarea").val(existingEvent);
+        }
+
+    });
 });
-
-
-
-
-
-
-// when page is refreshed, events persist
-
